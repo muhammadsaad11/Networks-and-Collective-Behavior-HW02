460d81726221fa35362e639ca86cc1ce62a281df
muhammadsaad11 <60126212+muhammadsaad11@users.noreply.github.com>
2022-02-27 19:39:24 +0500

    Add files via upload

--- q4_E-R_graph.py
+++ q4_E-R_graph.py
@@ -2,39 +2,6 @@ import networkx as nx
 from statistics import mean  
 import matplotlib.pyplot as plt
   
-# G = nx.erdos_renyi_graph(50,0.5, directed = False)
-# nx.draw(G, with_labels=True)
-# # plt.show()
-# print(G.number_of_edges())
-# print(G.nodes())
-# print(G.edges())
-# # for line in nx.generate_adjlist(G):
-# #     print(line)
-# # for v in G.nodes():
-# #     print(nx.clustering(G, v))
-# path_lengths = []
-# degrees = []
-# clustering = []
-
-# for node in G.nodes():
-#     degrees.append(G.degree(node))
-#     clustering.append(nx.clustering(G, node))
-#     for node2 in G.nodes():
-#         if node != node2:
-#             path_lengths.append(nx.shortest_path_length(G, node, node2))
-            
-# print(path_lengths)
-# print(degrees)
-# print(clustering)
-
-# print("Average Path Length:",mean(path_lengths))
-# print("Average Degree:",mean(degrees))
-# print("Average Clustering:",mean(clustering))
-
-# # print(nx.shortest_path_length(G, 2,16))
-# # print(nx.shortest_path_length(G, 16,2))
-# # print(nx.generate_adjlist(G))
-
 
 def Erdos_Reyni_graph(n, p):
     G = nx.erdos_renyi_graph(n,p, directed = False)
@@ -50,49 +17,9 @@ def Erdos_Reyni_graph(n, p):
         for node2 in G.nodes():
             if node != node2:
                 path_lengths.append(nx.shortest_path_length(G, node, node2))
-                
-    # print(path_lengths)
-    # print(degrees)
-    # print(clustering)
 
-    # print("Average Path Length:",mean(path_lengths))
-    # print("Average Degree:",mean(degrees))
-    # print("Average Clustering:",mean(clustering))
 
     return (mean(path_lengths),mean(degrees),mean(clustering))
-
-
-
-
-
-# def Erdos_Reyni_graph(n, p):
-#     G = nx.erdos_renyi_graph(n,p, directed = False)
-#     # nx.draw(G, with_labels=True)
-#     # plt.show()
-#     path_lengths = []
-#     degrees = []
-#     clustering = []
-
-#     nodes_g = G.nodes()
-#     print(nodes_g)
-#     for node in range(len(nodes_g)):
-#         print(nodes_g[2])
-#         degrees.append(G.degree(nodes_g[node]))
-#         clustering.append(nx.clustering(G, nodes_g[node]))
-#         for node2 in range(node, len(nodes_g)):
-#             print(nodes_g[node2])
-#             path_lengths.append(nx.shortest_path_length(G, nodes_g[node], nodes_g[node2]))
-                
-#     # print(path_lengths)
-#     # print(degrees)
-#     # print(clustering)
-
-#     # print("Average Path Length:",mean(path_lengths))
-#     # print("Average Degree:",mean(degrees))
-#     # print("Average Clustering:",mean(clustering))
-
-#     return (mean(path_lengths),mean(degrees),mean(clustering))
-
 
 
 
@@ -114,9 +41,7 @@ print("When n = 100 and p = 0.2")
 print("Average Clustering:",mean(Average_clustering))
 print("Average Path Length:", mean(Average_path_length))
 print("Average Degree:", mean(Average_degree_network))
-# plt.bar(degree_distribution_lst, degree_distribution_lst)
 
-# plt.show()
 plt.hist(degree_distribution_lst, len(degree_distribution_lst)//2)
 plt.show()
 
@@ -139,9 +64,7 @@ print("When n = 200 and p = 0.1")
 print("Average Clustering:",mean(Average_clustering))
 print("Average Path Length:", mean(Average_path_length))
 print("Average Degree:", mean(Average_degree_network))
-# plt.bar(degree_distribution_lst, degree_distribution_lst)
 
-# plt.show()
 plt.hist(degree_distribution_lst, len(degree_distribution_lst)//2)
 plt.show()
 
@@ -164,8 +87,6 @@ print("When n = 400 and p = 0.08")
 print("Average Clustering:",mean(Average_clustering))
 print("Average Path Length:", mean(Average_path_length))
 print("Average Degree:", mean(Average_degree_network))
-# plt.bar(degree_distribution_lst, degree_distribution_lst)
 
-# plt.show()
 plt.hist(degree_distribution_lst, len(degree_distribution_lst)//2)
 plt.show()
